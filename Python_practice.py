e76daa3a9888e3a3a0d5782529146c755367fcbd
Kyle Riley <rileyky1@gmail.com>
2022-04-06 08:54:00 -0400

    Adding PyPoll.py file.

---
+++ Python_practice.py
@@ -0,0 +1,25 @@
+counties = ["Arapahoe","Denver","Jefferson"]
+if counties[1] == 'Denver':
+    print(counties[1])
+
+
+
+temperature = int(input("What is the temperature outside? "))
+
+
+if temperature > 80:
+    print("Turn on the AC.")
+else:
+    print("Open the windows.")
+
+
+
+x = 0
+while x <= 5:
+    print(x)
+    x = x + 1
+
+
+
+
+    
